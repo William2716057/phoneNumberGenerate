5d8e8ee384678dd7a964dc70e5b79739cdc1b082
William <willspri@gmail.com>
2025-03-14 10:06:43 +1300

    changed to lambda

--- phoneNumberGenerate.py
+++ phoneNumberGenerate.py
@@ -1,10 +1,9 @@
 import random
 
-
 quantity = int(input("How many numbers? "))
 prefixes = ["021", "022", "027"]
 
+produce_numbers = lambda prefix: [print(f"{prefix} {random.randint(1000000000, 9999999999)}") for i in range(quantity)]
+
 for prefix in prefixes:
-    for _ in range(quantity):
-        number = random.randint(1000000000, 9999999999)  # Generate a 10-digit number
-        print(f"{prefix} {number}")
+    produce_numbers(prefix)
